278b3051bfc4cff3573a200536ff64850f3fce86
Aswin S <aswinsaswin25@gmail.com>
2022-11-20 13:28:50 +0530

    social media integration project completed

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return  const MaterialApp(
       debugShowCheckedModeBanner: false,
       
-      title: "TOLOTO",
+      title: "Yolana",
       home: Scaffold(
         body: Register(),
         
